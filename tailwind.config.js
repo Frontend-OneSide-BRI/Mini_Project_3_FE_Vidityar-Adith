52d2d7666dc871efeca2f9188bbcf1e4b4f99d54
Vidityar <van.adith@gmail.com>
2023-06-07 12:05:17 +0700

    add: Color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDarkBlue: "#343090",
+        primaryBlue: "#5f59f7",
+        primaryLightBlue: "#6592fd",
+        primaryBlueSKy: "#44c2fd",
+        primaryPurple: "#8c61ff",
+        dark: "#05011a",
+        secondaryGray: "#A5A4A4",
+        secondaryLightGray: "#D3D3D1",
+        secondaryDarkGray: "#635E5D",
+      },
+    },
   },
   plugins: [],
 };
