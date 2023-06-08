c5560ab729d8db5137a3adcced906d013bc70293
Vidityar <van.adith@gmail.com>
2023-06-08 20:36:29 +0700

    fix: props atom

--- src/components/Atom/Anchor/index.jsx
+++ src/components/Atom/Anchor/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-function Anchor({ textContent }) {
+function Anchor({ children, hrefLink }) {
   return (
     <>
       <a
-        href=""
+        href={hrefLink ? `${hrefLink}` : ""}
         className="block py-2 pl-3 pr-4 m-2 text-white border-transparent border-2
         bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 
         md:dark:text-primaryBlue md:hover:text-primaryLightBlue md:hover:border-primaryBlue"
         aria-current="page"
       >
-        {textContent}
+        {children}
       </a>
     </>
   );
--- src/components/Atom/Button/index.jsx
+++ src/components/Atom/Button/index.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 
-function Button({ textContent }) {
+function Button({ children, onClick }) {
   return (
     <>
       <button
         type="button"
-        class="font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white
-        bg-primaryDarkBlue hover:bg-primaryLightBlue
+        onClick={onClick ? () => onClick() : null}
+        className="font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 text-white 
+        bg-primaryDarkBlue hover:bg-primaryLightBlue 
         dark:bg-primaryDarkBlue dark:hover:bg-primaryBlue"
       >
-        {textContent}
+        {children}
       </button>
+    </>
+  );
+}
+
+export function ButtonOutline({ children, onClick }) {
+  return (
+    <>
+      <Button
+        className="bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white"
+        onClick={onClick ? () => onClick() : null}
+      >
+        {children}
+      </Button>
     </>
   );
 }
