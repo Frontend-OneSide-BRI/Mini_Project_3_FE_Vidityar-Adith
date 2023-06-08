3d3dc1cb381c1d9ffc32a31faee17a469a77b072
Vidityar <van.adith@gmail.com>
2023-06-08 20:37:46 +0700

    Add: Molecules Navbar or header

--- src/components/Molecules/Navbar/index.jsx
+++ src/components/Molecules/Navbar/index.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Button from "../../Atom/Button";
-import Anchor from "../../Atom/Anchor";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../../../assets/logo-text-dark.png";
 import Tabs from "../../Atom/Tabs";
@@ -26,22 +25,28 @@ function Navbar() {
 
   return (
     <>
-      <nav className="header fixed top-0 left-0 w-full z-99 transition duration-500 ease-in-out">
-        <div className="header__wrap flex items-center justify-between h-[100px] px-8">
+      <nav className="header fixed top-0 left-0 w-full z-[100] transition duration-500 ease-in-out bg-primaryBlue">
+        <div className="header__wrap flex items-center justify-between px-8">
           <div className="logo flex items-center">
             <Link to="/">
-              <img src={Logo} alt="logo" className="mr-2 w-[150px]" />
+              <img
+                src={Logo}
+                alt="logo"
+                className="mr-2 w-[150px] h-[80px] object-cover"
+              />
             </Link>
           </div>
           <div className="flex items-center justify-between h-[100px] px-8 fixed md:relative bottom-0">
             <ul className="header__nav flex items-center">
               {headerNav.map((e, i) => (
-                <Tabs items={e} index={i} active={active} />
+                <div key={i}>
+                  <Tabs items={e} index={i} active={active} />
+                </div>
               ))}
             </ul>
           </div>
           <div className="mt-3">
-            <Button textContent={"Get VMovie"} />
+            <Button> Get Movie</Button>
           </div>
         </div>
       </nav>
