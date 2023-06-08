22c6e55f9a6b06e89dc0e32fe0d6da7065200293
Vidityar <van.adith@gmail.com>
2023-06-08 12:27:38 +0700

    Add: Atom image

---
+++ src/components/Atom/Image/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Image() {
+  return (
+    <>
+      <figure class="max-w-lg mx-auto">
+        <img
+          class="h-auto max-w-full rounded-lg mx-auto"
+          src="https://images.unsplash.com/photo-1519751138087-5bf79df62d5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt="description"
+        />
+        {/* <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
+          Image caption
+        </figcaption> */}
+      </figure>
+    </>
+  );
+}
+
+export default Image;
