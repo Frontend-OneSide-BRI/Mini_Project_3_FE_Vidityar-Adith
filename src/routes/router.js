60ea1084f0e1d64399aa5010e7216bc6c698dda8
Vidityar <van.adith@gmail.com>
2023-06-08 20:33:03 +0700

    feat: layout to the router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routers from ".";
+import Layout from "../pages/Layout";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        {routers.map((item, index) => (
-          <Route key={index} path={item.path} element={item.element} />
-        ))}
+        {routers.map((item, index) =>
+          item.layout ? (
+            <Route
+              key={index}
+              path={item.path}
+              element={<Layout access={item.access}>{item.element}</Layout>}
+            />
+          ) : (
+            <Route key={index} path={item.path} element={item.element} />
+          )
+        )}
       </Routes>
     </BrowserRouter>
   );
