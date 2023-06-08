fb970e889e1a98a83339d17df4c54a30501b80c4
Vidityar <van.adith@gmail.com>
2023-06-08 20:32:07 +0700

    add: multiple function to hit the API

--- src/service/index.js
+++ src/service/index.js
@@ -1,18 +1,59 @@
-import http from "../helper/api";
+import http from "../helper/axiosClient";
 
-const headers = {
-  accept: "application/json",
-  Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NDA1ODk1ZWYwMzFiZmQwMmZlOWJjNjBlMDJmNzVhYiIsInN1YiI6IjY0N2VlY2E5OTM4MjhlMDBiZjlmZjIwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yxfKYpfAsVxFSp_a61vQhNbuqsujhT1an_Sv107R1cY",
+export const category = {
+  movie: "movie",
+  tv: "tv",
 };
 
-async function getMovie(setData, params = "") {
-  await http
-    .get(`movie/changes?${params}`, headers)
-    .then((res) => {
-      setData(res.data);
-    })
-    .catch((res) => {});
-}
+export const movieType = {
+  upcoming: "upcoming",
+  popular: "popular",
+  top_rated: "top_rated",
+};
 
-export { getMovie };
+export const tvType = {
+  popular: "popular",
+  top_rated: "top_rated",
+  on_the_air: "on_the_air",
+};
+
+const tmdbApi = {
+  getMoviesList: (type, params) => {
+    const url = "movie/" + movieType[type];
+    return http.get(url, params);
+  },
+  getDiscoverList: (params) => {
+    const url = "discover/movie/";
+    return http.get(url, params);
+  },
+  getTvList: (type, params) => {
+    const url = "tv/" + tvType[type];
+    return http.get(url, params);
+  },
+  getVideos: (cate, id) => {
+    const url = category[cate] + "/" + id + "/videos";
+    return http.get(url, { params: {} });
+  },
+  search: (cate, params) => {
+    const url = "search/" + category[cate];
+    return http.get(url, params);
+  },
+  detail: (cate, id, params) => {
+    const url = category[cate] + "/" + id;
+    return http.get(url, params);
+  },
+  credits: (cate, id) => {
+    const url = category[cate] + "/" + id + "/credits";
+    return http.get(url, { params: {} });
+  },
+  similar: (cate, id) => {
+    const url = category[cate] + "/" + id + "/similar";
+    return http.get(url, { params: {} });
+  },
+  genre: () => {
+    const url = "genre/movie/list?language=en";
+    return http.get(url, { params: {} });
+  },
+};
+
+export default tmdbApi;
